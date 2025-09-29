aff9dc95fd02f4c869af6c4f88ea9c5b9da1ddb9
Mark Luigi <markluigi@yahoo.it>
2025-09-29 22:07:09 +0200

    .

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ProductsResponse } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -6,11 +7,28 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   constructor() { }
 
-  async getProducts() {
+  async getProducts(): Promise<ProductsResponse> {
+    // Una funzione "asincrona" (async) è una funzione che:
+    // - restituisce sempre una Promise, anche se usi "return" normale
+    // - permette di usare "await" per aspettare il risultato di operazioni lente (es. fetch)
+    // - non blocca il resto del programma: mentre questa funzione aspetta, altre parti del codice possono continuare
+    // - ": Promise<ProductsResponse>" indica che restituisce una Promise
+    //    che, quando completata, conterrà un valore di tipo ProductsResponse
+
     const response = await fetch("https://dummyjson.com/products");
+    // "fetch" fa una richiesta HTTP (qui di tipo GET) all'URL indicato
+    // "await" blocca l'esecuzione finché non arriva la risposta
+    // "response" conterrà l'oggetto della risposta HTTP
+
     const data = await response.json();
+    // ".json()" legge il corpo della risposta e lo trasforma da JSON a oggetto JavaScript
+    // anche questo è asincrono, quindi serve "await"
+    // "data" conterrà i prodotti già pronti come oggetto/array
+
     console.log(data)
 
     return data;
+    // restituisce i dati a chi ha chiamato la funzione
+    // essendo async, in realtà torna una Promise che risolve con "data"
   }
 }
