6ee969e17d920f8807c7066e9664c49a950985e6
Mark Luigi <markluigi@yahoo.it>
2025-10-07 20:54:16 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { ProductList } from './components/product-list/product-list';
 import { Home } from './components/home/home';
 import { Contatti } from './components/contatti/contatti';
 
-// NOTA; cosa fa? e perchè?; cos'è path? che valori dargli. component serve per importare
+// L'array 'routes' definisce le rotte principali dell'app. 
+// Ogni oggetto ha una proprietà 'path' (il percorso nell'URL) e 'component' (il componente mostrato in quella rotta).
+// Esempio: quando l'URL è '/prodotti', Angular mostra il componente ProductList.
 export const routes: Routes = [
     {
         path: "prodotti",
